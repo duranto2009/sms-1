a2761f3f8622d8a5f492add3bb7e555dc65b2236
tarikmanoar <tarikmanoar@gmail.com>
2020-09-01 22:02:01 +0600

    DB design for student crud

---
+++ app/Http/Controllers/ClassTableController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ClassTable;
+use Illuminate\Http\Request;
+
+class ClassTableController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ClassTable  $classTable
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ClassTable $classTable)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ClassTable  $classTable
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ClassTable $classTable)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ClassTable  $classTable
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ClassTable $classTable)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ClassTable  $classTable
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ClassTable $classTable)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_09_01_155141_create_students_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('password');
+            $table->foreignId('guardian_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('classtable_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->string('section');
+            $table->date('dob')->nullable();
+            $table->string('gender');
+            $table->text('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
