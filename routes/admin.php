6b00e0834153d09b50609e7a90ed1b0ae447e323
tarikmanoar <tarikmanoar@gmail.com>
2020-09-09 22:32:42 +0600

    Fixed Class ROUTE Bug

--- routes/admin.php
+++ routes/admin.php
@@ -8,13 +8,13 @@ Route::view('widget','admin.partials.widget');
 Route::middleware(['role:admin','auth'])->prefix('dashboard')->namespace('Admin')->group(function () {
     Route::resource('admin', 'AdminController')->only('index');
     Route::resource('class', 'ClassTableController');
-    Route::get('read-data', 'ClassTableController@readData')->name('class.readData');
+    Route::get('read-data/class', 'ClassTableController@readData')->name('class.readData');
     Route::resource('guardian', 'GuardianController');
     Route::get('read-guardian', 'GuardianController@readData')->name('guardian.readData');
     Route::resource('student', 'StudentController');
     Route::get('student-section', 'StudentController@section')->name('student.section');
     Route::get('student-filer', 'StudentController@filter')->name('student.filter');
     Route::resource('session', 'SessionYearController');
-    Route::get('read-data', 'SessionYearController@readData')->name('session.readData');
+    Route::get('read-data/session', 'SessionYearController@readData')->name('session.readData');
     Route::get('session-active', 'SessionYearController@active')->name('session.active');
 });
