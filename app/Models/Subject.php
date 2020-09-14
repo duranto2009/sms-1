4b9e49abf0a6a10fe81622caa37c0d0a1af787dc
tarikmanoar <tarikmanoar@gmail.com>
2020-09-14 20:59:13 +0600

    Make Subject controller model

---
+++ app/Models/Subject.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Subject extends Model
+{
+    protected $guarded = ['id'];
+    public function class(){
+        return $this->belongsTo(ClassTable::class,'class_table_id');
+    }
+}
